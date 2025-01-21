2b29f7de68d6308a6da7d61bda21cc0f0615ae7d
connor123456789009876542545667 <160531887+connor123456789009876542545667@users.noreply.github.com>
2025-01-21 17:50:56 +0000

    menu.lua

--- menu.lua
+++ menu.lua
@@ -57,11 +57,11 @@ local themes = {
 }
 local themeStyles = {
     DarkTheme = {
-        SchemeColor = Color3.fromRGB(64, 64, 64),
-        Background = Color3.fromRGB(0, 0, 0),
-        Header = Color3.fromRGB(0, 0, 0),
-        TextColor = Color3.fromRGB(255,255,255),
-        ElementColor = Color3.fromRGB(20, 20, 20)
+    SchemeColor = Color3.fromRGB(210, 1, 3),
+    Background = Color3.fromRGB(0, 0, 0),
+    Header = Color3.fromRGB(28, 29, 34),
+    TextColor = Color3.fromRGB(255,255,255),
+    ElementColor = Color3.fromRGB(32, 32, 38)
     },
     LightTheme = {
         SchemeColor = Color3.fromRGB(150, 150, 150),
